ec5428f932a83df6bbfc0bcd43610c5f4d117da4
Wanli Zhang <lebuyou1688@gmail.com>
2024-01-10 01:22:04 +0700

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,36 +1,34 @@
-
 from modules import launch_utils
-tensor = tensor.to(device)
-args = launch_utils.args
-python = launch_utils.python
-git = launch_utils.git
-index_url = launch_utils.index_url
-dir_repos = launch_utils.dir_repos
-
-commit_hash = launch_utils.commit_hash
-git_tag = launch_utils.git_tag
-
-run = launch_utils.run
-is_installed = launch_utils.is_installed
-repo_dir = launch_utils.repo_dir
-
-run_pip = launch_utils.run_pip
-check_run_python = launch_utils.check_run_python
-git_clone = launch_utils.git_clone
-git_pull_recursive = launch_utils.git_pull_recursive
-list_extensions = launch_utils.list_extensions
-run_extension_installer = launch_utils.run_extension_installer
-prepare_environment = launch_utils.prepare_environment
-configure_for_tests = launch_utils.configure_for_tests
-start = launch_utils.start
-
+import torch
 
 def main():
+    args = launch_utils.args
+    tensor = torch.Tensor([1.0])
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    tensor = tensor.to(device)
+    
+    python = launch_utils.python
+    git = launch_utils.git
+    index_url = launch_utils.index_url
+    dir_repos = launch_utils.dir_repos
+    commit_hash = launch_utils.commit_hash
+    git_tag = launch_utils.git_tag
+    run = launch_utils.run
+    is_installed = launch_utils.is_installed
+    repo_dir = launch_utils.repo_dir
+    run_pip = launch_utils.run_pip
+    check_run_python = launch_utils.check_run_python
+    git_clone = launch_utils.git_clone
+    git_pull_recursive = launch_utils.git_pull_recursive
+    list_extensions = launch_utils.list_extensions
+    run_extension_installer = launch_utils.run_extension_installer
+    prepare_environment = launch_utils.prepare_environment
+    configure_for_tests = launch_utils.configure_for_tests
+    start = launch_utils.start
+
     if args.dump_sysinfo:
         filename = launch_utils.dump_sysinfo()
-
         print(f"Sysinfo saved as {filename}. Exiting...")
-
         exit(0)
 
     launch_utils.startup_timer.record("initial startup")
@@ -43,7 +41,6 @@ def main():
         configure_for_tests()
 
     start()
-
 
 if __name__ == "__main__":
     main()
